fc58d93565e76596a611af6ad424b8e09e656bfb
DLJ949 <djohnson949@yahoo.com>
2023-03-05 20:11:15 -0800

    added table to website

--- ps6_problem_set/app.R
+++ ps6_problem_set/app.R
@@ -3,44 +3,45 @@
 library(shiny)
 library(tidyverse)
 
-social_data <- read.delim("../WhatsgoodlyData-6.csv")
+social_data <- read_delim("../WhatsgoodlyData-6.csv")
+
+n_distinct(social_data$`Segment Description`)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
+  titlePanel("Problem Set 6: Social Media Marketing Effectiveness"),
+  p("This app uses survey data collected by Adam Halper from ", 
+    strong("Whatsgoodly,"), "a social polling company."),
+  p("This dataset contains ", em(nrow(social_data)), "responses 
+  from ", em(n_distinct(social_data$`Segment Description`)), " different questions 
+  from pollers in the Generation Z and Millennial age groups about if they had
+  purchased a product after seeing an ad on social media and their demographics."),
+  tabsetPanel(
+    tabPanel(
+      "About"
+    ),
+    tabPanel(
+      "Plot"
+    ),
+    tabPanel(
+      "Table",
+      dataTableOutput("filteredData")
     )
+  )
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
-    })
+  
+  # Filter the data so that statistics on which social medias had the most
+  # influence on ad purchases display
+  output$filteredData = renderDataTable({
+    social_data %>% 
+      select(Question, Answer, Count) %>% 
+      group_by(Answer) %>% 
+      summarise(total_votes = sum(unique(Count))) %>% 
+      arrange(rank(desc(total_votes)))
+  })
 }
 
 # Run the application 
